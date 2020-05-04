2da7b4a9750a702810a100f69768aa800c2272d3
EdgarRojas <edgarrojas13@hotmail.com>
2020-05-04 17:13:53 -0500

    Trabajando en la pantalla principal de perifericos, mostrar la imagen del producto

--- Simple_Stock/Controllers/PerifericosController.cs
+++ Simple_Stock/Controllers/PerifericosController.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Simple_Stock.cModels;
+using Simple_Stock.Models;
+using Newtonsoft.Json;
 
 namespace Simple_Stock.Controllers
 {
     public class PerifericosController : Controller
     {
+        public Models.simple_stockEntities db = new Models.simple_stockEntities(); //conexion al modelo de la base de datos
         // GET: Perifericos
         public ActionResult Index()
+        {
+            List<cCategorias> listaCategorias = null;
+            /*mediante entity framework, realizamos la consulta a la tabla que queramos mostrar 
+            los datos de la tabla*/
+            listaCategorias = (from p in db.categorias
+                            select new cCategorias
+                            {
+                                id_categoria = p.id_categoria,
+                                nombre_categoria = p.nombre_categoria
+                                                       
+                            }).ToList();
+            /*
+             SelectListItem nos sirve para mostrar que datos mostraran en nuestro dropdownlist de 
+             nuestra vista
+             */
+            List<SelectListItem> items = listaCategorias.ConvertAll(a =>
+            {
+                return new SelectListItem()
+                {
+                    Text = a.nombre_categoria.ToString(),
+                    Value = a.id_categoria.ToString(),
+                    Selected = false
+                };
+            });
+
+            ViewBag.items = items; //Viewbag se utiliza para enviar datos a la vista
+
+            return View();
+        }
+
+        public ActionResult Producto()
         {
             return View();
         }
---
+++ Simple_Stock/cModels/cPerifericos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple_Stock.cModels
+{
+    public class cPerifericos
+    {
+        public int id_producto { get; set; }
+        public string codigo_producto { get; set; }
+        public string nombre_producto { get; set; }
+        public string asignado { get; set; }
+        public int stock { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string serie { get; set; }
+        public string tipo { get; set; }
+        public string ubicacion { get; set; }
+        public string estado { get; set; }
+        public System.DateTime date_added { get; set; }
+        public string observaciones { get; set; }
+        public int id_categoria { get; set; }
+        public string url_image { get; set; }
+    }
+}
